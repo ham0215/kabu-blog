aa92a281ce9fffd0f740d9228f36e9dfe2ba4bf4
naoto hamada <hamada@vis-its.com>
2020-05-25 23:37:06 +0900

    Fix img path

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import logo from '../img/hamkabu.jpg'
+import logo from '/img/hamkabu.jpg'
 
 const Navbar = class extends React.Component {
   constructor(props) {
