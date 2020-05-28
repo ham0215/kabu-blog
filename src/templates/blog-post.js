a091471359caccead51b71eec9b497136e34440b
naoto hamada <hamada@vis-its.com>
2020-05-29 00:52:02 +0900

    Add widgets

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -40,6 +40,7 @@ export const BlogPostTemplate = ({
               </div>
             ) : null}
           </div>
+          <script type="text/javascript">amzn_assoc_ad_type ="responsive_search_widget"; amzn_assoc_tracking_id ="hamchance0215-22"; amzn_assoc_marketplace ="amazon"; amzn_assoc_region ="JP"; amzn_assoc_placement =""; amzn_assoc_search_type = "search_widget";amzn_assoc_width ="auto"; amzn_assoc_height ="auto"; amzn_assoc_default_search_category =""; amzn_assoc_default_search_key ="投資";amzn_assoc_theme ="light"; amzn_assoc_bg_color ="FFFFFF"; </script><script src="//z-fe.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&Operation=GetScript&ID=OneJS&WS=1&Marketplace=JP"></script>
         </div>
       </div>
     </section>
